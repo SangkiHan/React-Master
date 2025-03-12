113584978d4c9f0ec55989307a36bacd16eaaa6f
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-12 13:43:06 +0900

    feat: 공통으로 사용하는 css속성 묶기

--- src/App.js
+++ src/App.js
@@ -4,37 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
-  /* 
-  AS-IS
-  return (
-    <div style={{ display: "flex" }}>
-      <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
-      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
-    </div>
-  ); */
-
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Circle bgColor="teal" />
+      <Box bgColor="tomato" />
     </Father>
   );
 }
